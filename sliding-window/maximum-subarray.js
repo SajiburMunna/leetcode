cd1fc7c6e77fd9b0bc75c8d2c730be2791858a7a
sajiburmunna <sajiburmunna087@gmail.com>
2025-07-19 22:04:18 +0600

    feat(sliding-window): maximum subarray

---
+++ sliding-window/maximum-subarray.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var maxSubArray = function (nums, k = 4) {
+  let maxSum = 0;
+  for (let i = 0; i < nums.length; i++) {
+    if (i <= k - 1) {
+      maxSum = maxSum + nums[i];
+    }
+    if (i > k - 1) {
+      let sum = maxSum - nums[i - k] + nums[i];
+      if (sum > maxSum) {
+        maxSum = sum;
+      }
+    }
+  }
+  return maxSum;
+};
+
+maxSubArray([1, 12, -5, -6, 50, 3]);
